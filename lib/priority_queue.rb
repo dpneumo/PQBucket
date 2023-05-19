6bc05696df41e7707a4eea14654561cb23dc89f2
Mitchell Kuppinger <dpneumo@gmail.com>
2023-05-18 19:07:33 -0500

    Switch to using Items with a priority attribute that implement comparison with <=> on priority.

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -1,78 +1,84 @@
+# This is basically a bucket queue
+# Items in the queue can be required to be unique
+# insert: O(1), find_highest: O(#priorities), pull_highest: O(#priorities)
 class PriorityQueue
-  # This is basically a bucket queue
-  # All items in the queue are unique
-  attr_accessor :items, :priorities, :q
+  attr_reader :items, :priorities, :q
   def initialize
     @items = [] # maintain as an ordered list of unique items
     @priorities = [] # maintain as an ordered list
     @q = Hash.new {|hash, priority| hash[priority] = [] } # hash values: lists of indexes into items
   end
 
-  def insert(item, priority)
-    items << item if ! items.include? item
-    q[priority] << (items.size - 1)
-    ndx = priorities.bsearch_index{|p| priority >= p } || -1
-    priorities.insert(ndx, priority) if priority != priorities[ndx]
+  def insert(item)
+    # Uncomment if queue member uniqueness required
+    # return if items.any? {|member| member.equal? item }
+    @items << item
+    priority = item.priority
+    @q[priority] << (@items.size - 1)
+    ndx = @priorities.bsearch_index{|p| priority >= p } || -1
+    @priorities.insert(ndx, priority) if priority != @priorities[ndx]
   end
+  alias_method :<<, :insert
 
   def pull_highest
-    pull_item(priorities.first)
+    pull_item(@priorities.first)
   end
 
   def pull_lowest
-    pull_item(priorities.last)
+    pull_item(@priorities.last)
   end
 
   def find_highest
     return nil if empty?
-    max_priority_ndx = q[priorities.first].first
-    items[max_priority_ndx]
+    max_priority_ndx = @q[@priorities.first].first
+    @items[max_priority_ndx]
   end
 
   def find_lowest
     return nil if empty?
-    min_priority_ndx = q[priorities.last].last
-    items[min_priority_ndx]
+    min_priority_ndx = @q[@priorities.last].last
+    @items[min_priority_ndx]
   end
 
+  def empty?
+    @priorities.empty?
+  end
+
+  def clear
+    @items.clear
+    @priorities.clear
+    @q.clear
+  end
+
+# Extensions
   def find_by_priority(priority)
-    q[priority].map {|ndx| items[ndx] }.first
+    @q[priority].map {|ndx| @items[ndx] }.first
   end
 
   def find_by_label_and_priority(label, priority)
-    q[priority].reduce([]) do |found, ndx|
-      found << items[ndx] if items[ndx].to_s == label
+    @q[priority].reduce([]) do |found, ndx|
+      found << @items[ndx] if @items[ndx].label == label
       found
     end.first
   end
 
   def find_by_label(label)
-    q.reduce([]) do |found, (priority, ndxs)|
-      selected_ndxs = ndxs.select {|ndx| items[ndx] == label }
-      found.concat(selected_ndxs.map {|ndx| items[ndx] })
+    @q.reduce([]) do |found, (priority, ndxs)|
+      selected_ndxs = ndxs.select {|ndx| @items[ndx].label == label }
+      found.concat(selected_ndxs.map {|ndx| @items[ndx] })
     end.first
-  end
-
-  def empty?
-    priorities.empty?
-  end
-
-  def clear
-    items.clear
-    priorities.clear
-    q.clear
   end
 
 private
   def pull_item(priority)
     return nil if empty?
-    item_ndx = q[priority].shift
-    remove(priority) if q[priority].empty?
-    items[item_ndx]
+    item_ndx = @q[priority].shift
+    remove(priority) if @q[priority].empty?
+    @items[item_ndx]
   end
 
   def remove(priority)
-    q.delete(priority)
-    priorities.delete(priority)
+    @q.delete(priority)
+    @priorities.delete(priority)
   end
 end
--- tests/priority_queue_test.rb
+++ tests/priority_queue_test.rb
@@ -2,16 +2,17 @@
 
 require_relative 'test_helper'
 require_relative '../lib/priority_queue'
+require_relative '../lib/item'
 
 class PriorityQueueTest < Minitest::Test
   def setup
     @pq = PriorityQueue.new
-    @pq.insert('b', 2)
-    @pq.insert('x', 6)
-    @pq.insert('y', 6)
-    @pq.insert('z', 3)
-    @pq.insert('w', 3)
-    @pq.insert('r', 10)
+    @pq.insert(Item.new(label: 'b', priority: 2))
+    @pq.insert(Item.new(label: 'x', priority: 6))
+    @pq.insert(Item.new(label: 'y', priority: 6))
+    @pq.insert(Item.new(label: 'z', priority: 3))
+    @pq.insert(Item.new(label: 'w', priority: 3))
+    @pq.insert(Item.new(label: 'r', priority: 10))
   end
 
   def test_inits_priorities_to_empty_array
@@ -31,45 +32,50 @@ class PriorityQueueTest < Minitest::Test
     assert pq.empty?
   end
 
+  def test_an_alias_for_insert # <<
+    @pq << Item.new(label: 'm', priority: 20)
+    assert_equal 'm', @pq.pull_highest.label
+  end
+
   def test_correctly_inserts_multiple_items
     expected = {2=>[0], 6=>[1, 2], 3=>[3, 4], 10=>[5]}
     assert_equal expected, @pq.q
-    assert_equal ['b', 'x', 'y', 'z', 'w', 'r'], @pq.items
+    assert_equal ['b', 'x', 'y', 'z', 'w', 'r'], @pq.items.map {|itm| itm.label }
     assert_equal [10, 6, 3, 2], @pq.priorities
     assert ! @pq.empty?
   end
 
   def test_correctly_pulls_highest_items_returning_nil_when_empty
-    assert_equal 'r', @pq.pull_highest
-    assert_equal 'x', @pq.pull_highest
-    assert_equal 'y', @pq.pull_highest
-    assert_equal 'z', @pq.pull_highest
-    assert_equal 'w', @pq.pull_highest
-    assert_equal 'b', @pq.pull_highest
+    assert_equal 'r', @pq.pull_highest.label
+    assert_equal 'x', @pq.pull_highest.label
+    assert_equal 'y', @pq.pull_highest.label
+    assert_equal 'z', @pq.pull_highest.label
+    assert_equal 'w', @pq.pull_highest.label
+    assert_equal 'b', @pq.pull_highest.label
     assert_nil    @pq.pull_highest
     assert @pq.empty?
     assert_equal Hash.new, @pq.q
   end
 
   def test_correctly_pulls_lowest_items_returning_nil_when_empty
-    assert_equal 'b', @pq.pull_lowest
-    assert_equal 'z', @pq.pull_lowest
-    assert_equal 'w', @pq.pull_lowest
-    assert_equal 'x', @pq.pull_lowest
-    assert_equal 'y', @pq.pull_lowest
-    assert_equal 'r', @pq.pull_lowest
+    assert_equal 'b', @pq.pull_lowest.label
+    assert_equal 'z', @pq.pull_lowest.label
+    assert_equal 'w', @pq.pull_lowest.label
+    assert_equal 'x', @pq.pull_lowest.label
+    assert_equal 'y', @pq.pull_lowest.label
+    assert_equal 'r', @pq.pull_lowest.label
     assert_nil    @pq.pull_lowest
     assert @pq.empty?
     assert_equal Hash.new, @pq.q
   end
 
   def test_can_mix_pull_highest_and_pull_lowest
-    assert_equal 'r', @pq.pull_highest
-    assert_equal 'b', @pq.pull_lowest
-    assert_equal 'x', @pq.pull_highest
-    assert_equal 'z', @pq.pull_lowest
-    assert_equal 'y', @pq.pull_highest
-    assert_equal 'w', @pq.pull_lowest
+    assert_equal 'r', @pq.pull_highest.label
+    assert_equal 'b', @pq.pull_lowest.label
+    assert_equal 'x', @pq.pull_highest.label
+    assert_equal 'z', @pq.pull_lowest.label
+    assert_equal 'y', @pq.pull_highest.label
+    assert_equal 'w', @pq.pull_lowest.label
     assert_nil    @pq.pull_highest
     assert_equal Hash.new, @pq.q
   end
@@ -80,9 +86,9 @@ class PriorityQueueTest < Minitest::Test
   end
 
   def test_find_highest_returns_the_highest_priority_item
-    assert_equal 'r', @pq.find_highest
+    assert_equal 'r', @pq.find_highest.label
     @pq.pull_highest
-    assert_equal 'x', @pq.find_highest
+    assert_equal 'x', @pq.find_highest.label
   end
 
   def test_find_lowest_returns_nil_for_empty_queue
@@ -91,13 +97,13 @@ class PriorityQueueTest < Minitest::Test
   end
 
   def test_find_lowest_returns_the_lowest_priority_item
-    assert_equal 'b', @pq.find_lowest
+    assert_equal 'b', @pq.find_lowest.label
     @pq.pull_lowest
-    assert_equal 'w', @pq.find_lowest
+    assert_equal 'w', @pq.find_lowest.label
   end
 
   def test_find_by_priority_returns_first_inserted_with_that_priority
-    assert_equal "z", @pq.find_by_priority(3)
+    assert_equal "z", @pq.find_by_priority(3).label
   end
 
   def test_find_by_priority_returns_nil_if_item_not_found
@@ -105,7 +111,7 @@ class PriorityQueueTest < Minitest::Test
   end
 
   def test_find_by_label_and_priority_returns_item_with_label_and_priority
-    assert_equal "w", @pq.find_by_label_and_priority('w', 3)
+    assert_equal "w", @pq.find_by_label_and_priority('w', 3).label
   end
 
   def test_find_by_label_and_priority_returns_nil_if_item_not_found
@@ -115,7 +121,7 @@ class PriorityQueueTest < Minitest::Test
   end
 
   def test_find_by_label_returns_first_inserted_item_with_label
-    assert_equal "z", @pq.find_by_label('z')
+    assert_equal "z", @pq.find_by_label('z').label
   end
 
   def test_find_by_label_returns_nil_if_label_not_found
@@ -127,4 +133,5 @@ class PriorityQueueTest < Minitest::Test
     assert @pq.empty?
     assert @pq.q.empty?
   end
+
 end
